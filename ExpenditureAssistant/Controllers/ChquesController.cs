8f6e700e2afb12ddc2a07219ce5c043744231986
Duah Kwaku <ibrahimkwakuduah@hotmail.com>
2018-06-12 17:04:06 +0000

    Cheques controller

--- ExpenditureAssistant/Controllers/ChquesController.cs
+++ ExpenditureAssistant/Controllers/ChquesController.cs
@@ -7,64 +7,64 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ExpenditureAssistant.Controllers
 {
-    public class DepartmentsController : Controller
+    public class ChequesController : Controller
     {
         private readonly DbContextOptions<ApplicationDbContext> dco;
 
-        public DepartmentsController(DbContextOptions<ApplicationDbContext> options) => dco = options;
+        public ChequesController(DbContextOptions<ApplicationDbContext> options) => dco = options;
 
         [HttpGet]
-        public async Task<IEnumerable> List() => await new ApplicationDbContext(dco).Departments.Select(x => new { x.DepartmentsID, x.Department, x.Concurrency, Amount = x.Expenditure.Sum(t => t.Cheques.Amount) }).ToListAsync();
+        public async Task<IEnumerable> List() => await new ApplicationDbContext(dco).Cheques.Include(x => x.Expenditures.Departments).Select(x => new { x.Amount, x.ChequeNumber, x.ChequesID, x.DateIssued, x.Expenditures.Departments.Department }).ToListAsync();
 
         [HttpGet]
         public async Task<IActionResult> Find(int id)
         {
-            var dept = await new ApplicationDbContext(dco).Departments.FindAsync(id);
+            var dept = await new ApplicationDbContext(dco).Cheques.FindAsync(id);
             return dept == null ? (IActionResult)NotFound(new { Message = "Department not found" }) : Ok(dept);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody]Departments dept)
+        public async Task<IActionResult> Create([FromBody]Cheques chq)
         {
             if (!ModelState.IsValid)
                 return BadRequest(new { Error = "Invalid data was submitted", Message = ModelState.Values.First(x => x.Errors.Count > 0).Errors.Select(t => t.ErrorMessage).ToList() });
             using (var db = new ApplicationDbContext(dco))
             {
-                if (await db.Departments.AnyAsync(x => x.Department == dept.Department))
-                    return BadRequest(new { Message = "Department already exists" });
-                db.Add(dept);
+                if (await db.Cheques.AnyAsync(x => x.ChequeNumber == chq.ChequeNumber))
+                    return BadRequest(new { Message = "Cheque already exists" });
+                db.Add(chq);
                 await db.SaveChangesAsync();
-                return Created($"/Departments/Find?id={dept.DepartmentsID}", dept);
+                return Created($"/Cheques/Find?id={chq.ChequesID}", chq);
             }
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit([FromBody]Departments dept)
+        public async Task<IActionResult> Edit([FromBody]Cheques chq)
         {
             if (!ModelState.IsValid)
                 return BadRequest(new { Error = "Invalid data was submitted", Message = ModelState.Values.First(x => x.Errors.Count > 0).Errors.Select(t => t.ErrorMessage).ToList() });
             using (var db = new ApplicationDbContext(dco))
             {
-                if (!await db.Departments.AnyAsync(x => x.DepartmentsID == dept.DepartmentsID))
+                if (!await db.Cheques.AnyAsync(x => x.ChequesID == chq.ChequesID))
                     return BadRequest(new { Message = "Department does not exists" });
-                db.Entry(dept).State = EntityState.Modified;
+                db.Entry(chq).State = EntityState.Modified;
                 await db.SaveChangesAsync();
-                return Ok(dept);
+                return Ok(chq);
             }
         }
 
         [HttpPost]
-        public async Task<IActionResult> Delete([FromBody]Departments dept)
+        public async Task<IActionResult> Delete([FromBody]Cheques chq)
         {
             if (!ModelState.IsValid)
                 return BadRequest(new { Error = "Invalid data was submitted", Message = ModelState.Values.First(x => x.Errors.Count > 0).Errors.Select(t => t.ErrorMessage).ToList() });
             using (var db = new ApplicationDbContext(dco))
             {
-                if (!await db.Departments.AnyAsync(x => x.DepartmentsID == dept.DepartmentsID))
+                if (!await db.Cheques.AnyAsync(x => x.ChequesID == chq.id  ))
                     return BadRequest(new { Message = "Department does not exists" });
-                db.Entry(dept).State = EntityState.Deleted;
+                db.Entry(chq).State = EntityState.Deleted;
                 await db.SaveChangesAsync();
-                return Ok(dept);
+                return Ok(chq);
             }
         }
     }
